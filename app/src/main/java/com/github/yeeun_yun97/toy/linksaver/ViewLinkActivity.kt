0f7ed0c07f5e6f767409930a1f9dcdf2b2375342
yyn9704@gmail.com <yyn9704@gmail.com>
2022-04-04 01:01:16 +0900

    ViewLinkActivity layout

---
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ViewLinkActivity.kt
@@ -0,0 +1,11 @@
+package com.github.yeeun_yun97.toy.linksaver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewLinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_link)
+    }
+}
