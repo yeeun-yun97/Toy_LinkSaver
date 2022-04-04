c5952fc3992210d01cb660cb52ccfe2230a0cb5b
yyn9704@gmail.com <yyn9704@gmail.com>
2022-04-05 00:27:03 +0900

    add main clickListener to move to LinkViewActivity

--- app/src/main/java/com/github/yeeun_yun97/toy/linksaver/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.github.yeeun_yun97.toy.linksaver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.MainActivity_textView)
+        textView.setOnClickListener {
+            startActivity(Intent(this,ViewLinkActivity::class.java))
+        }
     }
 }
