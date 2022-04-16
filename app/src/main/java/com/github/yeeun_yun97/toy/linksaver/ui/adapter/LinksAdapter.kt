5d0daecfcb04896f38b0a97884a1c45fb9072d7c
yyn9704@gmail.com <yyn9704@gmail.com>
2022-04-16 21:25:46 +0900

    viewbinding to recyclerView

--- app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ui/adapter/LinksAdapter.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ui/adapter/LinksAdapter.kt
@@ -7,38 +7,33 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.github.yeeun_yun97.toy.linksaver.databinding.ItemLinksBinding
 
 class LinksAdapter(private val itemList: LiveData<ArrayList<SjLink>>, private val openOperation: (String)->Unit) :
     RecyclerView.Adapter<LinksViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LinksViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_links, parent, false)
-        return LinksViewHolder(view);
+        val binding = ItemLinksBinding.inflate(LayoutInflater.from(parent.context))
+        return LinksViewHolder(binding);
     }
 
     override fun onBindViewHolder(holder: LinksViewHolder, position: Int) {
         holder.setLink(itemList.value!![position],openOperation)
     }
-
     override fun getItemCount(): Int = itemList.value!!.size
 
 }
 
-class LinksViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val domainTextView: TextView by lazy { itemView.findViewById(R.id.linksItem_domainTextView) }
-    private val nameTextView: TextView by lazy { itemView.findViewById(R.id.linksItem_nameTextView) }
-    private val webButton: ImageView by lazy { itemView.findViewById(R.id.linksItem_webButton) }
-    private val editButton: ImageView by lazy { itemView.findViewById(R.id.linksItem_editButton) }
-    private val deleteButton: ImageView by lazy { itemView.findViewById(R.id.linksItem_deleteButton) }
+class LinksViewHolder(private val binding:ItemLinksBinding) : RecyclerView.ViewHolder(binding.root) {
     private lateinit var item: SjLink
 
     fun setLink(item: SjLink, openOperation: (String) -> Unit) {
         this.item = item
-        domainTextView.setText(item.domain.name)
-        nameTextView.setText(item.name)
-        webButton.setOnClickListener { openOperation(item.fullUrl) }
-        editButton.setOnClickListener { editLink() }
-        deleteButton.setOnClickListener { deleteLink() }
+        binding.linksItemDomainTextView.setText(item.domain.name)
+        binding.linksItemNameTextView.setText(item.name)
+        binding.linksItemWebButton.setOnClickListener { openOperation(item.fullUrl) }
+        binding.linksItemEditButton.setOnClickListener { editLink() }
+        binding.linksItemDeleteButton.setOnClickListener { deleteLink() }
     }
 
     private fun editLink() {
