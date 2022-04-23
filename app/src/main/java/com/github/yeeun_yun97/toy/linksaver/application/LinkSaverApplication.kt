a6a0613874cf73175cd4add43ccc859e23bbbec3
yyn9704@gmail.com <yyn9704@gmail.com>
2022-04-23 19:15:33 +0900

    changed SjCompanion have the logic to set singleton db instance

--- app/src/main/java/com/github/yeeun_yun97/toy/linksaver/application/LinkSaverApplication.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/application/LinkSaverApplication.kt
@@ -1,7 +1,6 @@
 package com.github.yeeun_yun97.toy.linksaver.application
 
 import android.app.Application
-import androidx.room.Room
 import com.github.yeeun_yun97.toy.linksaver.data.db.SjDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -10,15 +9,22 @@ import kotlinx.coroutines.launch
 class LinkSaverApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        val db =
-            Room.databaseBuilder(applicationContext, SjDatabase::class.java, "sj_database").build()
-        SjDatabase.db = db
+
+        //open Database
+        SjDatabase.openDatabase(applicationContext)
+
+        //insert initial Data
         GlobalScope.launch(Dispatchers.IO) {
-            //SjDatabase.insertFirstData()
+            SjDatabase.insertFirstData()
         }
     }
 
     override fun onTerminate() {
         super.onTerminate()
+
+        //close Database
+        //사실 어디서 닫아야 할 지 잘 모르겠다.
+        //백그라운드로 갔을 때도 닫아야 할까?
+        SjDatabase.closeDatabase()
     }
 }
--- app/src/main/java/com/github/yeeun_yun97/toy/linksaver/data/db/SjDatabase.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/data/db/SjDatabase.kt
@@ -1,6 +1,9 @@
 package com.github.yeeun_yun97.toy.linksaver.data.db
 
+import android.content.Context
+import android.util.Log
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.github.yeeun_yun97.toy.linksaver.data.dao.SjDao
 import com.github.yeeun_yun97.toy.linksaver.data.model.LinkTagCrossRef
@@ -8,32 +11,50 @@ import com.github.yeeun_yun97.toy.linksaver.data.model.SjDomain
 import com.github.yeeun_yun97.toy.linksaver.data.model.SjLink
 import com.github.yeeun_yun97.toy.linksaver.data.model.SjTag
 
-@Database(entities = [SjTag::class, SjLink::class, SjDomain::class, LinkTagCrossRef::class], version = 1)
+@Database(
+    entities = [SjTag::class, SjLink::class, SjDomain::class, LinkTagCrossRef::class],
+    version = 1
+)
 abstract class SjDatabase : RoomDatabase() {
     companion object {
         lateinit var db: SjDatabase
 
+        fun openDatabase(applicationContext: Context) {
+            if(this::db.isInitialized) {
+                Log.i(javaClass.canonicalName,"there is already initialized database instance")
+            }else{
+                this.db =
+                    Room.databaseBuilder(applicationContext, SjDatabase::class.java, "sj_database")
+                        .build()
+            }
+        }
 
-
+        fun closeDatabase() {
+            if (!db.isOpen) {
+                Log.i(javaClass.canonicalName, "attempt to close Database that is not open")
+            } else {
+                this.db.close()
+            }
+        }
 
         fun insertFirstData() {
             val dao = db.getDao()
 
-            var youtube = SjDomain(0,"유튜브", "https://m.youtube.com/watch?")
-            var allchan = SjDomain(1, "올찬식탁", "https://m.smartstore.naver.com/allchanfood/products/")
+            var youtube = SjDomain(0, "유튜브", "https://m.youtube.com/watch?")
+            var allchan =
+                SjDomain(1, "올찬식탁", "https://m.smartstore.naver.com/allchanfood/products/")
             dao.insertDomain(youtube)
             dao.insertDomain(allchan)
 
-            var bread = SjTag(0,"빵")
+            var bread = SjTag(0, "빵")
             dao.insertTag(bread)
 
-
-            var pokemonBread = SjLink(0,"돌아온 포켓몬빵", allchan.did, "6333541088")
-            var pieceCake = SjLink(1,"조각 케이크", allchan.did, "718907248")
-            var ytnNews = SjLink(2,"포켓몬빵 뉴스", youtube.did, "v=GJ3a33gdx7o")
-            dao.insertLinkTagCrossRef(LinkTagCrossRef(pokemonBread.lid,bread.tid))
-            dao.insertLinkTagCrossRef(LinkTagCrossRef(pieceCake.lid,bread.tid))
-            dao.insertLinkTagCrossRef(LinkTagCrossRef(ytnNews.lid,bread.tid))
+            var pokemonBread = SjLink(0, "돌아온 포켓몬빵", allchan.did, "6333541088")
+            var pieceCake = SjLink(1, "조각 케이크", allchan.did, "718907248")
+            var ytnNews = SjLink(2, "포켓몬빵 뉴스", youtube.did, "v=GJ3a33gdx7o")
+            dao.insertLinkTagCrossRef(LinkTagCrossRef(pokemonBread.lid, bread.tid))
+            dao.insertLinkTagCrossRef(LinkTagCrossRef(pieceCake.lid, bread.tid))
+            dao.insertLinkTagCrossRef(LinkTagCrossRef(ytnNews.lid, bread.tid))
             dao.insertLink(pokemonBread)
             dao.insertLink(pieceCake)
             dao.insertLink(ytnNews)
