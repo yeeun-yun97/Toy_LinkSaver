845164357e207c6fd16a94fa8cb7ff4a81b97daf
yyn9704@gmail.com <yyn9704@gmail.com>
2022-04-28 14:56:55 +0900

    added app icon and set splash

--- app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ui/activity/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ui/activity/MainActivity.kt
@@ -5,15 +5,24 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.github.yeeun_yun97.toy.linksaver.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.MainActivity_textView)
-        textView.setOnClickListener {
-            startActivity(Intent(this, ViewLinkActivity::class.java))
+        setContentView(R.layout.activity_splash)
+        CoroutineScope(Dispatchers.Default).launch{
+            val wait = launch{ delay(1200)}
+            wait.join()
+            launch(Dispatchers.Main){
+                val intent : Intent = Intent(applicationContext,ViewLinkActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
     }
 
