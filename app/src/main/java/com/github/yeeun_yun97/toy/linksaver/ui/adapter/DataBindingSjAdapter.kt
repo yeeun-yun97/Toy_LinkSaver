1b1bb90f534aec487e809ee08d9f4fee316b14e5
yyn9704@gmail.com <yyn9704@gmail.com>
2022-05-22 23:09:40 +0900

    change listFragment and searchFragment ui

--- app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ui/adapter/DataBindingSjAdapter.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/linksaver/ui/adapter/DataBindingSjAdapter.kt
@@ -1,5 +1,6 @@
 package com.github.yeeun_yun97.toy.linksaver.ui.adapter
 
+import android.view.View
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
@@ -8,28 +9,43 @@ import com.github.yeeun_yun97.toy.linksaver.ui.component.SjTagChip
 import com.google.android.material.chip.ChipGroup
 
 class DataBindingSjAdapter {
-    companion object{
+    companion object {
         @JvmStatic
         @BindingAdapter("chipCountData")
-        fun setTextByCount(view: TextView, count:Int){
-            if(count<2){
-                view.isVisible= false
-            }else{
-                view.isVisible=true
-                view.setText("및 ${count-1}개")
+        fun setTextByCount(view: TextView, count: Int) {
+            if (count < 2) {
+                view.isVisible = false
+            } else {
+                view.isVisible = true
+                view.setText("및 ${count - 1}개")
             }
         }
 
         @JvmStatic
         @BindingAdapter("chipDataList")
-        fun setChipByList(view: ChipGroup, tags:List<SjTag>){
+        fun setChipByList(view: ChipGroup, tags: List<SjTag>) {
             view.removeAllViews()
-            if(tags.isNotEmpty()){
-                val chip = SjTagChip(view.context,tags[0])
-                chip.isCheckable=false
-                chip.isClickable=false
-                view.addView(chip)
+            if (tags.isNotEmpty()) {
+                for( tag in tags) {
+                    val chip = SjTagChip(view.context, tag)
+                    chip.isChecked = true
+                    chip.isCheckable = false
+                    chip.isClickable = false
+                    view.addView(chip)
+                }
             }
+        }
+
+        @JvmStatic
+        @BindingAdapter("textOrHide")
+        fun setTextOrHide(view: TextView, string: String) {
+            if (string.isEmpty()) {
+                view.visibility = View.GONE
+            } else {
+                view.setText(string)
+                view.visibility = View.VISIBLE
+            }
+
         }
     }
 }
